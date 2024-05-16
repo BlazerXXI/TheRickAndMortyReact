3ccc0306a6a51c28fb9f2721adf3e0f4ad8589ba
BlazerXXI <ruslandendik@gmail.com>
2024-05-16 23:09:41 +0300

    fix: remove a tag

--- src/components/CharacterCard/CharacterModal.tsx
+++ src/components/CharacterCard/CharacterModal.tsx
@@ -80,7 +80,7 @@ const CharacterModal = (props: {
 									className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
 									onClick={() => toggleModal()}
 								>
-									<a href="#">Close</a>
+									Close
 								</button>
 							</div>
 						</div>
